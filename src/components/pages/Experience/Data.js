79a3587b3c6b2aed69a1b1fe48971b3bb7e73898
Tess Gompper <tgomp9@gmail.com>
2021-07-27 23:20:59 -0400

    oopsies

--- src/components/pages/Experience/Data.js
+++ src/components/pages/Experience/Data.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import HeroSection from '../../HeroSection/HeroSection';
-import {experienceFord, experienceObjTwo} from '../Experience/Data';
+export const experienceFord ={
+    lightBg: false,
+    date: '06/24/2019 - 09/15/2019',
+    projectName: 'Product Management Intern at Ford Motor Company',
+    projectTags: 'FordPass, In App Guidance, High School Science Technology Program',
+    description: 'As an HSSTP intern I worked with the FordPass Guides team to learn about what goes into an app such as FordPass on the Product Management side of things. I then led indusrty research, value comparison, and business plan design for an in-app guidance feature in the FordPass app with my fellow HSSTP intern.',
+    imgStart:'',
+    img: '',
+    alt: 'image'
+};
 
-function Experience() {
-    return (
-        <>
-           <HeroSection {...experienceFord}/>
-           <HeroSection {...experienceObjTwo}/>
-        </>
-    )
-}
-
-export default Experience
+export const experienceObjTwo ={
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    date: '00/00/0000',
+    projectName: 'Experience name',
+    projectTags: 'tags',
+    description: 'experience description',
+    imgStart:'',
+    img: '',
+    alt: 'img'
+};
