e13a267db5bb39dd36a4fb2d0d463b476eb19c1f
Tess Gompper <tgomp9@gmail.com>
2021-07-27 22:45:24 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Footer from './components/Footer/Footer';
+import Projects from './components/pages/Projects/Projects';
+import Experience from './components/pages/Experience/Experience';
+import Home from './components/pages/Home/Home';
+import About from './components/pages/About/About';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar/>
+      <Switch>
+        <Route path = '/' exact component ={Home}/>
+        <Route path = '/projects' component= {Projects}/>
+        <Route path = '/experience' component= {Experience}/>
+        <Route path = '/about' component = {About}/>
+      </Switch>
+      <Footer/>
+    </Router>
   );
 }
 
