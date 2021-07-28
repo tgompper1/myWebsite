63bf1db022743fdda0bb59b79cad961783ad0d60
Tess Gompper <tgomp9@gmail.com>
2021-07-27 22:51:51 -0400

    initial commit

---
+++ src/Components/pages/About/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './About.css'
+
+function About() {
+    return (
+        <div class='bubble__container'>
+            <div class='bubble__container2'>
+                <div class='bubble'>
+                    <h1>About Me</h1>  
+                    <p>blah blah blah</p>          
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default About
