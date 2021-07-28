5dc2f97b22e52b9827886627385f8f07e7898140
Tess Gompper <tgomp9@gmail.com>
2021-07-28 00:12:01 -0400

    troubleshooting

--- src/App.js
+++ src/App.js
@@ -10,18 +10,17 @@ import About from './components/pages/About/About';
 
 function App() {
   return (
-    <header><h1>yuh</h1></header>
-   // <Router>
-      
-     // <Navbar/>
- //     <Switch>
-   //     <Route path = '/' exact component ={Home}/>
-     //   <Route path = '/projects' component= {Projects}/>
-       // <Route path = '/experience' component= {Experience}/>
- //       <Route path = '/about' component = {About}/>
-   //   </Switch>
-     // <Footer/>
-   // </Router>
+    //<header><h1>yuh</h1></header>
+    <Router>
+      <Navbar/>
+      <Switch>
+        <Route path = '/' exact component ={Home}/>
+        <Route path = '/projects' component= {Projects}/>
+        <Route path = '/experience' component= {Experience}/>
+        <Route path = '/about' component = {About}/>
+      </Switch>
+      <Footer/>   
+    </Router>
   );
 }
 
