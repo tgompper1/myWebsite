7eb43b7c214d7c7ede3b07e432554d5eaf27ce29
Tess Gompper <tgomp9@gmail.com>
2021-07-27 22:54:10 -0400

    initial commit

---
+++ src/Components/pages/Home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+    return (
+        <div class='bubble__container'>
+            <div class = 'bubble__container2'>
+            <div class='bubble'>
+                <h1>Welcome</h1>  
+                <p>blah blah blah</p>          
+            </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
