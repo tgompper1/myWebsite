aca604607f30660073c4094b0a4a740316ad0ccc
Tess Gompper <tgomp9@gmail.com>
2021-07-27 22:55:02 -0400

    initial commit

---
+++ src/Components/pages/Projects/Data.js
@@ -0,0 +1,23 @@
+export const projectObjOne ={
+    lightBg: false,
+    date: 'June 2021',
+    projectName: 'TessGompper website',
+    projectTags: 'Figma, React JS, JS, CSS',
+    description: 'To introduce myself to Web Development I designed and developed this site to use as an online portfolio and resume. I protoptyed my site using Figma and created my color palette with Coolors. Before fully diving into this project I introduced myself to HTML, CSS and JavaScript on CodePen by trying different ways to make Navigation bars. Finally, after watching some tutorials, I developed my first responsive website! I plan on updating and improving my site as I learn more about Web Development.',
+    imgStart:'',
+    img: '',
+    alt: 'image'
+};
+
+export const projectObjTwo ={
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    date: '00/00/0000',
+    projectName: 'project name',
+    projectTags: 'tags, tech, tools, skills',
+    description: 'project description',
+    imgStart:'',
+    img: '',
+    alt: 'img'
+};
---
+++ src/Components/pages/Projects/Projects.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import HeroSection from '../../HeroSection/HeroSection';
+import {projectObjOne, projectObjTwo} from '../Projects/Data';
+
+/* eventually add menu of projects, drop down menu, snap scroll*/
+function Projects() {
+    return (
+        <>
+           <HeroSection {...projectObjOne}/>
+           <HeroSection {...projectObjTwo}/>
+        </>
+    )
+}
+
+export default Projects
