ee226043de9ec5d81ede6f40acc6a77a707d3f76
Tess Gompper <tgomp9@gmail.com>
2021-07-28 20:00:00 -0400

    added about

--- src/components/pages/About/About.js
+++ src/components/pages/About/About.js
@@ -7,7 +7,11 @@ function About() {
             <div class='bubble__container2'>
                 <div class='bubble'>
                     <h1>About Me</h1>  
-                    <p>blah blah <a href="https://www.cs.mcgill.ca/"><nobr>My long sentence</nobr></a> bah</p>          
+                    <p>Hi, my name is Tess Gompper and I am a current second year student majoring in 
+                        computer science at <a href="https://www.cs.mcgill.ca/academic/undergrad/ba/"><nobr>McGill University</nobr></a> with an 
+                        interest in web and mobile development I am also minoring in 
+                        <a href ="https://www.mcgill.ca/ahcs/undergraduate/cs"><nobr>communications studies</nobr></a>
+                        to gain an uderstanding of the impact of communication technologies on people throughout history.<br></br>Originally from Ann Arbor, MI </p>          
                 </div>
             </div>
         </div>
