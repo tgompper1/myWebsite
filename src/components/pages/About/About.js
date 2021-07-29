803e3b2a0d87f7b4e9abb08b83eed7d9fe60f544
Tess Gompper <tgomp9@gmail.com>
2021-07-28 20:07:10 -0400

    update about

--- src/components/pages/About/About.js
+++ src/components/pages/About/About.js
@@ -9,9 +9,12 @@ function About() {
                     <h1>About Me</h1>  
                     <p>Hi, my name is Tess Gompper and I am a current second year student majoring in 
                         computer science at <a href="https://www.cs.mcgill.ca/academic/undergrad/ba/"><nobr>McGill University</nobr></a> with an 
-                        interest in web and mobile development I am also minoring in 
-                        <a href ="https://www.mcgill.ca/ahcs/undergraduate/cs"><nobr>communications studies</nobr></a>
-                        to gain an uderstanding of the impact of communication technologies on people throughout history.<br></br>Originally from Ann Arbor, MI </p>          
+                        interest in web and mobile development I am also minoring in  
+                        <a href ="https://www.mcgill.ca/ahcs/undergraduate/cs"><nobr>communications studies </nobr></a>
+                        to gain an uderstanding of the impact of communication technologies on people throughout history.<br></br>Originally from Ann Arbor, Michigan, I now
+                        live in Montreal and look forward to exploring more of the world. In my spare time 
+                        I enjoy hiking and exploring, reading, spending time with my cat, and of course learning more about programming!<br></br> 
+                        At McGill I am a member of the Figure Skating Team and can't wait to find more opportunities to be a part of the McGill community.</p>          
                 </div>
             </div>
         </div>
