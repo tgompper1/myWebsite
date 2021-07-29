c5c631f0dede8ac601ba892c109ba604da64d89a
Tess Gompper <tgomp9@gmail.com>
2021-07-28 20:20:39 -0400

    added welcome

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -6,8 +6,9 @@ function Home() {
         <div class='bubble__container'>
             <div class = 'bubble__container2'>
             <div class='bubble'>
-                <h1>Welcome</h1>  
-                <p>blah blah blah</p>          
+                <h1>Welcome!</h1>  
+                <p>I'm Tess and I am a second year computer science student at McGill University! 
+                    This website was my firt personal project and I will continue to improve and update it as I learn more!</p>          
             </div>
             </div>
         </div>
