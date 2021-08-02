026d03ad3cd3d309a9d7dfdaa06bcaa4ac75b18e
Tess Gompper <tgomp9@gmail.com>
2021-08-02 19:33:06 -0400

    update projects

--- src/components/pages/Projects/Data.js
+++ src/components/pages/Projects/Data.js
@@ -4,7 +4,7 @@ export const projectWebsite ={
     projectName: 'TessGompper website',
     projectTags: 'Figma, React JS, JS, CSS',
     description: 'To introduce myself to Web Development I designed and developed this site to use as an online portfolio and resume. I protoptyed my site using Figma and created my color palette with Coolors. Before fully diving into this project I introduced myself to HTML, CSS and JavaScript on CodePen by trying different ways to make Navigation bars. Finally, after watching some tutorials, I developed my first responsive website! I plan on updating and improving my site as I learn more about Web Development.',
-    imgStart:'start',
+    imgStart:'row-reverse',
     img: '',
     alt: 'image'
 };
@@ -16,8 +16,8 @@ export const projectShine ={
     date: '06/08/2020',
     projectName: 'Shine',
     projectTags: 'Android Studio, Figma, API, JSON, hackHer',
-    description: 'This project was from my first hackathon, hackHer, put on by the Queens Women in Computing group in November of 2020. For this virtual hackathon I worked with 3 other students to create a prototype for an app that would tell users what the best time of each day would be to get outside and enjoy the best weather. Inspired by my own experiences, this app was aimed at students stuck in their dorm all day doing online school who, at the end of the day, woudl come to realize they hadn &apos t gotten any fresh air or sunshine that day. Using android studio and the Open Weather Map API I programmed the part of the app that calculated which time of day had the best weather based on temperature and weather conditions.  ',
-    imgStart:'row reverse',
+    description: 'This project was from my first hackathon, hackHer, put on by the Queens Women in Computing group in November of 2020. For this virtual hackathon I worked with 3 other students to create a prototype for an app that would tell users what the best time of each day would be to get outside and enjoy the best weather. Inspired by my own experiences, this app was aimed at students stuck in their dorm all day doing online school who, at the end of the day, woudl come to realize they had not gotten any fresh air or sunshine that day. Using android studio and the Open Weather Map API I programmed the part of the app that calculated which time of day had the best weather based on temperature and weather conditions. This hackathon introduced me to Android Studio, Postman, APIs, and JSON files. Although we did not complete our app as we had hoped, hackHer was a wonderful first hackathon experience that introduced me to lots of new concepts and people from around the world!',
+    imgStart:'start',
     img: '',
     alt: 'img'
 };
