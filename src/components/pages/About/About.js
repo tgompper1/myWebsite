cbe36c3175e947ebf71279e07b63cf2c95f1a164
Tess Gompper <tgomp9@gmail.com>
2021-08-02 18:28:45 -0400

    update about me

--- src/components/pages/About/About.js
+++ src/components/pages/About/About.js
@@ -7,14 +7,27 @@ function About() {
             <div class='aboutbubble__container2'>
                 <div class='aboutbubble'>
                     <h1>About Me</h1>  
-                    <p>Hi, my name is Tess Gompper and I am a current second year student majoring in 
+                    <p>Hi, my name is Tess Gompper and I am from Ann Arbor, Michigan. Now living in Montreal I am a current second year student majoring in 
                         computer science at <a href="https://www.cs.mcgill.ca/academic/undergrad/ba/"><nobr>McGill University</nobr></a> with an 
-                        interest in web and mobile development I am also minoring in  
+                        interest in web and mobile development. I am also minoring in  
                         <a href ="https://www.mcgill.ca/ahcs/undergraduate/cs"><nobr> communications studies </nobr></a>
-                        to gain an uderstanding of the impact of communication technologies on people throughout history.<br></br>Originally from Ann Arbor, Michigan, I now
-                        live in Montreal and look forward to exploring more of the world. In my spare time 
-                        I enjoy hiking and exploring, reading, spending time with my cat, and of course learning more about programming!<br></br> 
-                        At McGill I am a member of the Figure Skating Team and can't wait to find more opportunities to be a part of the McGill community.</p>          
+                        to gain an uderstanding of the impact of communication technologies on people throughout history.<br></br> 
+                        I was first introduced to computer science as a member of my high school Innovative Vehicle Design team. Working with two friends and a mentor from Ford Motor Company 
+                        I programmed our vehicles ultrasonic blind spot detector and Inertial Measurment Unit. Being a part of this team
+                        taught me to love challenges and trying new things, it also gave me first hand experience of being a minority
+                        in STEM as a woman and has since motivated to me seek out as many opportunities as possible to meet more women like me. In my future at McGill I hope to work
+                        with the Women in CS club to create a more inclusive environment for women in CS at McGill!
+                        <br></br> I pride myself in on being a very creative person, I enjoy drawing and paiting as well as sewing and needlepoint. As a figure skater
+                        I get to combine this creativity with precision which I find relates strongly to computer science: though creating something technical and precise 
+                        a programmer also gets the opportunity to make something unique and beautiful. This combination of precision and creativity
+                        is one of the reasons I fell in love with Computer Science. I also consider myself to be very quick to learn. I am good at following directions 
+                        and asking for help which makes me easy to teach. This past summer working at a coffee shop I was able to progress from cashier to fully
+                        trained barista in just 1 week of training. Though a simple example, this showcases my ability to learn quickly and efficiently. <br></br>
+                        In my spare time I enjoy hiking and exploring, reading, spending time with my cat, and trying out new food spots! I have figure skated almost my whole and 
+                        am continuing skating for fun as a member of the McGill Figure Skating team. Throughout high school I worked with 
+                        special needs athletes through figure skating as a coach and Unified Partner with Michigan Special Olympics and I am excited to continue 
+                        my volunteering in Montreal. This fall I will be starting as a mentor with <a href= "https://www.montrealbeyondme.ca/"><nobr>Beyond Me Montreal</nobr> working</a>
+                         one-on-one with a child with special needs! </p>          
                 </div>
             </div>
         </div>
