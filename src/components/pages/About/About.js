5ce6b22c5e8819d3c373ee8356bb5d2532eb1d39
Tess Gompper <tgomp9@gmail.com>
2022-08-21 17:00:30 -0400

    update about me

--- src/components/pages/About/About.js
+++ src/components/pages/About/About.js
@@ -8,26 +8,24 @@ function About() {
                 <div class='aboutbubble'>
                     <h1>About Me</h1>  
                     <p>
-                        <p>Hi, my name is Tess Gompper and I am from Ann Arbor, Michigan. Now living in Montreal I am a current second year student majoring in 
+                        <p>Hi, my name is Tess Gompper and I am from Ann Arbor, Michigan. Now living in Montreal, I am a current third year student majoring in 
                         computer science at <a href="https://www.cs.mcgill.ca/academic/undergrad/ba/"><nobr>McGill University</nobr></a> with an 
-                        interest in web and mobile development. I am also minoring in  
+                        interest in web, mobile, and data development. I am also minoring in  
                         <a href ="https://www.mcgill.ca/ahcs/undergraduate/cs"><nobr> communications studies </nobr></a>
                         to gain an uderstanding of the impact of communication technologies on people throughout history.</p>
                         <p>I was first introduced to computer science as a member of my high school Innovative Vehicle Design team. Working with two friends and a mentor from Ford Motor Company 
                         I programmed our vehicles ultrasonic blind spot detector and Inertial Measurment Unit. Being a part of this team
                         taught me to love challenges and trying new things, it also gave me my first experience of being a minority
-                        in STEM as a woman. Now, as a member of <a href = "https://www.mcwics.com/"><nobr>McWiCS</nobr></a>, <a href = "https://rewritingthecode.org/"><nobr>Rewriting the Code</nobr></a>, <a href = "https://www.thewomens.network/"><nobr>The Women's Network</nobr></a> and <a href = "https://www.linkedin.com/company/technolgap/?originalSubdomain=ca"><nobr>Technolgap </nobr></a> 
+                        in STEM as a woman. Now, as an executive member of <a href = "https://www.mcwics.com/"><nobr>McWiCS</nobr></a>, and a community member of <a href = "https://rewritingthecode.org/"><nobr>Rewriting the Code</nobr></a>, <a href = "https://www.thewomens.network/"><nobr>The Women's Network</nobr></a> and <a href = "https://www.linkedin.com/company/technolgap/?originalSubdomain=ca"><nobr>Technolgap </nobr></a> 
                         I'm always looking for more opportunities to meet and learn from more women in STEM!</p>
                         <p>I pride myself in on being a very creative person, I enjoy drawing and paiting as well as sewing and needlepoint. As a figure skater
                         I get to combine this creativity with precision which I find relates strongly to computer science: though creating something technical and precise 
                         a programmer also gets the opportunity to make something unique and beautiful. This combination of precision and creativity
                         is one of the reasons I fell in love with Computer Science. I also consider myself to be very quick to learn. I am good at following directions 
-                        and asking for help which makes me easy to teach. This past summer working at a coffee shop I was able to progress from cashier to fully
-                        trained barista in just 1 week of training. Though a simple example, this showcases my ability to learn quickly and efficiently. </p>
+                        and asking for help which makes me easy to teach. </p>
                         <p>In my spare time I enjoy hiking and exploring, reading, spending time with my cat, and trying out new food spots! I have figure skated almost my whole and 
-                        am continuing skating for fun as a member of the McGill Figure Skating team. Throughout high school I worked with 
-                        special needs athletes through figure skating as a coach and Unified Partner with Michigan Special Olympics and I am excited to continue 
-                        my volunteering in Montreal. This fall I will be starting as a mentor with <a href= "https://www.montrealbeyondme.ca/"><nobr>Beyond Me Montreal</nobr></a> working 
+                        am continuing skating for fun as a member of the McGill Figure Skating team, where I am also VP Events. Throughout high school I worked with 
+                        special needs athletes through figure skating as a coach and Unified Partner with Michigan Special Olympics and I volunteered as a mentor with <a href= "https://www.montrealbeyondme.ca/"><nobr>Beyond Me Montreal</nobr></a> working 
                          one-on-one with a child with special needs! </p> 
 
                     </p>         
--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -7,7 +7,7 @@ function Home() {
             <div class = 'homebubble__container2'>
                 <div class='homebubble'>
                     <h1>Welcome!</h1>  
-                    <p>I'm Tess and I am a second year computer science student at McGill University! 
+                    <p>I'm Tess and I am a third year computer science student at McGill University! 
                         This website was my first personal project and I will continue to improve and update it as I learn more!</p>          
                 </div>
              </div>
